98d9e912dc34ce22c3f44ac51c81d2a7dab5f791
dfguarinm <48227625+dfguarinm@users.noreply.github.com>
2024-08-29 18:14:08 -0500

    feat(ui): add gesture recognition system and energy bar
    - Implemented a simple gesture recognition system extracted from an addon.
    - Added an energy bar (MP) that decreases as gestures are drawn.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,20 @@
 extends AnimatedSprite2D
 
+@onready var gesture_instance = $Camera2D/CanvasLayer/Gesture
+@onready var MAXhp = 5
+@onready var MAXmp = 5
+@onready var mpbar = $Camera2D/CanvasLayer/ProgressBar
 
 # Tamaño de cada tile
 var tile_size = Vector2(16, 16)
+
+func _ready() -> void:
+	gesture_instance.maxLengthLine = MAXmp*50
+	mpbar.max_value = MAXmp*50
+	mpbar.value = MAXmp*50
+
+func _physics_process(delta: float) -> void:
+	mpbar.value = MAXmp*50 - gesture_instance.current_length
 
 func _input(event):
 	if event is InputEventKey:
@@ -17,9 +29,12 @@ func _input(event):
 				KEY_RIGHT:
 					move_in_direction(Vector2(1, 0))
 	elif event is InputEventMouseButton and event.pressed:
-		var mouse_position = get_global_mouse_position()
-		var direction = (mouse_position - global_position).normalized()
-		move_in_direction(Vector2(round(direction.x), round(direction.y)))
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			var mouse_position = get_global_mouse_position()
+			var direction = (mouse_position - global_position).normalized()
+			move_in_direction(Vector2(round(direction.x), round(direction.y)))
+		elif event.button_index == MOUSE_BUTTON_RIGHT:
+			gesture_instance.classify()
 
 func move_in_direction(direction):
 	var new_position = global_position + direction * tile_size
